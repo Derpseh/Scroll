104b0a7cc778988986ab04ba6b6daa07143efd02
Derpseh <valkynora@gmail.com>
2024-07-26 10:48:51 +0200

    Update scroll.py
    finally getting me off my ass to change tab/space indent settings for n++

--- scroll/scroll.py
+++ scroll/scroll.py
@@ -76,7 +76,7 @@ class Scroll(commands.Cog):
 		global lbDict
 		global queueDict
 		global delayTime
-        global lbRegDict
+		global lbRegDict
 		#the dict of active recruiters and backlog are both keyed by the region that's being recruited/backlogged for for convenience
 		#here we're just seeing if there's enough for a full bucket, spitting it out if so, and sending a boilerplate if not
 		for key in recDict:
@@ -125,7 +125,7 @@ class Scroll(commands.Cog):
 		global activeQueue
 		global current4
 		global regionWhiteList
-        global lbRegDict
+		global lbRegDict
 		#once again logging for session end/forcestop purposes
 		current4 = asyncio.current_task()
 		#grabbing every new found since the last time a similar ping was made
@@ -290,7 +290,7 @@ class Scroll(commands.Cog):
 		global current
 		global current2
 		global delayTime
-        global lbRegDict
+		global lbRegDict
 		author = ctx.author
 		lbPath = await self.CheckPath(ctx, "leaderboards.txt")
 		lbRegPath = await self.CheckPath(ctx, "regionboards.txt")
